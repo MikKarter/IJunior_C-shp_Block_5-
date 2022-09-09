758f337fa33cafbf181a12b3c57370ecd19bb07b
MikKarter <66348029+MikKarter@users.noreply.github.com>
2022-09-09 14:36:24 +0900

    Add files via upload

---
+++ 5.Task_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> vocabulary = new Dictionary<string, string>();
+            vocabulary.Add("Word", "value");
+
+            string userInput = Console.ReadLine();
+
+            if (vocabulary.ContainsKey(userInput))
+            {
+                Console.WriteLine(vocabulary[userInput]);
+            }
+
+            else
+            {
+                Console.WriteLine("Wrong word, please try again");
+            }
+        }
+    }
+}
