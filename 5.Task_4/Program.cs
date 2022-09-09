bd3f6698b2697bfc1a74a21ece3042827b7b1c87
MikKarter <66348029+MikKarter@users.noreply.github.com>
2022-09-09 23:28:48 +0900

    Add files via upload

---
+++ 5.Task_4/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isWork = true;
+
+            List<string> dossier = new List<string>();
+            List<string> position = new List<string>();
+
+            while (isWork)
+            {
+                Console.WriteLine("Введите номер команды");
+                Console.WriteLine("1 - Добавить досье");
+                Console.WriteLine("2 - Показать все досье");
+                Console.WriteLine("3 - Удалить выбранное досье");
+                Console.WriteLine("4 - Выход из программы");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        AddDossier(ref dossier, ref position);
+                        break;
+                    case "2":
+                        ShowDossier(dossier, position);
+                        break;
+                    case "3":
+                        DeleteDossier(ref dossier, ref position);
+                        break;
+                    case "4":
+                        isWork = !isWork;
+                        break;
+                }
+            }
+        }
+        static void AddDossier(ref List<string> dossier, ref List<string> position)
+        {
+            Console.WriteLine("FullName");
+            string userInput = Console.ReadLine();
+            dossier.Add(userInput);
+            Console.WriteLine("Position");
+            userInput = Console.ReadLine();
+            position.Add(userInput);
+        }
+
+        static void DeleteDossier(ref List<string> dossier, ref List<string> position)
+        {
+            Console.WriteLine("Number of dossier");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            dossier.RemoveAt(userInput-1);
+            position.RemoveAt(userInput-1);
+
+        }
+
+        static void ShowDossier(List<string> dossier, List<string> position)
+        {
+            for (int i = 0; i < dossier.Count; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + dossier[i] + " " + position[i]);
+            }
+        }
+
+    }
+}
---
+++ 5.Task_4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("5.Task_4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("5.Task_4")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("773f7289-ba72-46c0-b9c6-44637de25802")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
