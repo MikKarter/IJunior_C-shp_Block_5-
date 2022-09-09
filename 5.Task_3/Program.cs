fc49c4173dd12c2e09b5ea5a3ec1773f8f333c82
MikKarter <66348029+MikKarter@users.noreply.github.com>
2022-09-09 22:36:04 +0900

    Add files via upload

---
+++ 5.Task_3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Тфыл_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {           
+            List<int> numbers = new List<int>();
+            GetNumber(ref numbers);
+        }
+
+        static void GetNumber(ref List<int> numbers)
+        {
+            bool isWork = true;  
+
+            while (isWork)
+            {     
+                string userInput = Console.ReadLine();
+                int.TryParse(userInput, out int resaultParse);
+
+                if (resaultParse != 0)
+                {
+                    numbers.Add(resaultParse);
+                }
+                else if (userInput == "sum")
+                {
+                    Console.WriteLine(numbers.Sum());
+                }
+                else if (userInput == "exit")
+                {
+                    isWork = false;
+                }
+                else
+                {
+                    Console.WriteLine("Input error. Please, input sum or exit for actions:");
+                }
+            }
+        }
+    }
+}
